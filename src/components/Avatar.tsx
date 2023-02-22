05927e6f62030c0165f91d2d03c558c90289955e
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-02-22 16:39:24 -0300

    passando os props para que ele pegue todos os atributos possiveis dentro do atributo imagem

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,15 +1,16 @@
+import { ImgHTMLAttributes } from "react"
 import styles from "./Avatar.module.css"
 
-interface AvatarProps{
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{
     hasBorder?: boolean,
-    src: string,
-    alt?: string
+   
 }
-export function Avatar({hasBorder = true, src}: AvatarProps){
+export function Avatar({hasBorder = true, ...props}: AvatarProps){
 
     return(
         <div>
-             <img className={hasBorder ? styles.avatarWithBorder : styles.avatar} src={src} />
+             <img className={hasBorder ? styles.avatarWithBorder : styles.avatar}  
+             {...props}/>
         </div>
        
     )
